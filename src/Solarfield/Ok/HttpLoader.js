d0001a4b42fd367a45b7a2a2701f93133bb25755
mike <mikecaines@gmail.com>
2015-12-31 22:46:33 -0400

    Implement Http/Css/JsonLoader which provide a Promise based api, etc.

---
+++ src/Solarfield/Ok/HttpLoader.js
@@ -0,0 +1,69 @@
+define(
+	'solarfield/lightship-js/src/Solarfield/Ok/HttpLoader',
+	[
+		'solarfield/ok-kit-js/src/Solarfield/Ok/ok',
+		'solarfield/ok-kit-js/src/Solarfield/Ok/HttpMux'
+	],
+	function (Ok, HttpMux) {
+		"use strict";
+
+		//FUTURE: HttpMux queuing/pool/reuse to avoid many parallel requests, etc.
+		//FUTURE: automatic retries
+
+		/**
+		 * @class Solarfield.Ok.HttpLoader
+		 * Wrapper for HttpMux, which provides a Promise based API.
+		 */
+		var HttpLoader = Ok.extendObject(Object, {
+			_getMux: function () {
+				if (!this._httpMux) {
+					this._httpMux = new HttpMux();
+				}
+
+				return this._httpMux;
+			},
+
+			/**
+			 * Loads data from aUrl.
+			 * @param {string} aUrl The url to load from.
+			 * @param {Object} [aOptions] Additional options.
+			 * @param {string} [aOptions.responseType] @see XMLHttpRequest.responseType
+			 * @returns {Promise}
+			 */
+			load: function (aUrl, aOptions) {
+				var options;
+
+				this.abort();
+
+				options = Ok.objectAssign({
+					responseType: ''
+				}, aOptions);
+
+				return new Promise(resolve => {
+					this._getMux().send({
+						url: aUrl,
+						responseType: options.responseType,
+						onEnd: function (aEvt) {
+							resolve(aEvt.response);
+						}
+					});
+				});
+			},
+
+			abort: function () {
+				if (this._httpMux) {
+					this._httpMux.abort();
+				}
+			},
+
+			constructor: function () {
+				this._httpMux = null;
+			}
+		});
+
+		Ok.defineNamespace('Solarfield.Ok');
+		Solarfield.Ok.HttpLoader = HttpLoader;
+
+		return HttpLoader;
+	}
+);
---
+++ src/Solarfield/Ok/JsonLoader.js
@@ -0,0 +1,81 @@
+define(
+	'solarfield/lightship-js/src/Solarfield/Ok/JsonLoader',
+	[
+		'solarfield/ok-kit-js/src/Solarfield/Ok/ok',
+		'solarfield/ok-kit-js/src/Solarfield/Ok/HttpLoader'
+	],
+	function (Ok, HttpLoader) {
+		"use strict";
+
+		/**
+		 * @class Solarfield.Ok.JsonLoader
+		 * @extends Solarfield.Ok.HttpLoader
+		 */
+		var JsonLoader = Ok.extendObject(HttpLoader, {
+			/**
+			 * Loads JSON from aUrl, and optionally performs post-load success checks.
+			 * @param {string} aUrl The url to load.
+			 * @param {object} [aOptions] Additional options.
+			 * @param {string} [aOptions.successKey] Dot-separated path which must exist in response json.
+			 * @param {string} [aOptions.successValue] Value at successKey must match this value.
+			 * @returns {Promise}
+			 */
+			load: function (aUrl, aOptions) {
+				var options = Ok.objectAssign({
+					successKey: null,
+					successValue: null
+				}, aOptions);
+
+				return JsonLoader.super.prototype.load(aUrl, options).then(responseText => {
+					var responseJson, json, error;
+
+					try {responseJson = JSON.parse(responseText);} catch (ex) {}
+
+					if (responseJson !== undefined) {
+						if (options.successValue) {
+							if (Ok.objectGet(responseJson, options.successKey) == options.successValue) {
+								json = responseJson;
+							}
+							else {
+								error = "successValue '" + options.successValue + "' at successKey '" + options.successKey + "' not found in response.";
+							}
+						}
+
+						else if (options.successKey) {
+							if (Ok.objectHas(responseJson, options.successKey)) {
+								json = responseJson;
+							}
+							else {
+								error = "successKey '" + options.successKey + "' not found in response.";
+							}
+						}
+
+						else {
+							json = responseJson;
+						}
+					}
+
+					else {
+						error = "Response could not be parsed as JSON."
+					}
+
+					if (error) {
+						return Promise.reject({
+							message: error,
+							response: responseText
+						});
+					}
+
+					else {
+						return json;
+					}
+				});
+			}
+		});
+
+		Ok.defineNamespace('Solarfield.Ok');
+		Solarfield.Ok.JsonLoader = JsonLoader;
+
+		return JsonLoader;
+	}
+);
