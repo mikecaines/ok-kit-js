7b2bd540a8424f9bb8f4994b5800b9aa8bb976bb
mike <mikecaines@gmail.com>
2016-01-23 09:45:25 -0400

    StructProxy: simplify definition

--- src/Solarfield/Ok/StructProxy.js
+++ src/Solarfield/Ok/StructProxy.js
@@ -24,84 +24,86 @@
 	"use strict";
 
 	/** @class Solarfield.Ok.StructProxy */
-	var StructProxy = function (aData) {
-		this._ohm_data = (aData != null && aData.constructor === Object) ? aData : {};
-	};
+	var StructProxy = Ok.extendObject(null, {
+		constructor: function (aData) {
+			this._ohm_data = (aData != null && aData.constructor === Object) ? aData : {};
+		},
 
-	StructProxy.prototype.getData = function () {
-		return this._ohm_data;
-	};
+		getData: function () {
+			return this._ohm_data;
+		},
 
-	StructProxy.prototype.get = function (aPath) {
-		return Ok.objectGet(this._ohm_data, aPath);
-	};
+		get: function (aPath) {
+			return Ok.objectGet(this._ohm_data, aPath);
+		},
 
-	StructProxy.prototype.getAsString = function (aPath) {
-		var value = this.get(aPath);
-		if (value == null) value = '';
-		else value = value.toString();
-		return value;
-	};
+		getAsString: function (aPath) {
+			var value = this.get(aPath);
+			if (value == null) value = '';
+			else value = value.toString();
+			return value;
+		},
 
-	StructProxy.prototype.getAsObject = function (aPath) {
-		var value = this.get(aPath);
-		var isNull = value == null;
-		var obj, k;
+		getAsObject: function (aPath) {
+			var value = this.get(aPath);
+			var isNull = value == null;
+			var obj, k;
 
-		if (!isNull) {
-			if (value.constructor === Object) {
-				obj = value;
+			if (!isNull) {
+				if (value.constructor === Object) {
+					obj = value;
+				}
+
+				else {
+					obj = {};
+
+					for (k in value) {
+						obj[k] = value[k];
+					}
+				}
 			}
 
 			else {
 				obj = {};
-
-				for (k in value) {
-					obj[k] = value[k];
-				}
 			}
-		}
 
-		else {
-			obj = {};
-		}
+			return obj;
+		},
 
-		return obj;
-	};
+		getAsArray: function (aPath) {
+			var value = this.get(aPath);
+			var isNull = value == null;
+			var arr, k;
 
-	StructProxy.prototype.getAsArray = function (aPath) {
-		var value = this.get(aPath);
-		var isNull = value == null;
-		var arr, k;
+			if (!isNull) {
+				if (value.constructor === Array) {
+					arr = value;
+				}
 
-		if (!isNull) {
-			if (value.constructor === Array) {
-				arr = value;
+				else {
+					arr = [];
+
+					for (k in value) {
+						arr.push(value[k]);
+					}
+				}
 			}
 
 			else {
 				arr = [];
-
-				for (k in value) {
-					arr.push(value[k]);
-				}
 			}
+
+			return arr;
+		},
+
+		getAsBool: function (aPath) {
+			return this.get(aPath) == true;
+		},
+
+		set: function (aPath, aValue) {
+			Ok.objectSet(this._ohm_data, aPath, aValue);
 		}
-
-		else {
-			arr = [];
-		}
-
-		return arr;
-	};
-
-	StructProxy.prototype.getAsBool = function (aPath) {
-		return this.get(aPath) == true;
-	};
-
-	StructProxy.prototype.set = function (aPath, aValue) {
-		Ok.objectSet(this._ohm_data, aPath, aValue);
-	};
+	});
 
 	Ok.defineNamespace('Solarfield.Ok');
 	return Solarfield.Ok.StructProxy = StructProxy;
