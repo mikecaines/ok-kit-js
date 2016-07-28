bfc0d14395c451666903520735a274e6d8cd19fd
mike <mikecaines@gmail.com>
2016-07-28 20:59:52 -0300

    add DomUtils::selectOptionsByValue()

--- src/Solarfield/Ok/DomUtils.js
+++ src/Solarfield/Ok/DomUtils.js
@@ -142,6 +142,60 @@
 		return ancestor;
 	};
 
+	DomUtils.selectOptionsByValue = function (aElement, aValues) {
+		var multiple = aElement.multiple;
+		var i, j, matched, values, selected, msg;
+
+		values = aValues instanceof Array ? [].concat(aValues) : [aValues];
+
+		if (!multiple && values.length > 1) {
+			msg = "Could not select multiple values";
+			if (aElement.id) msg += " in <select>[id='" + aElement.id + "']";
+			else if (aElement.name) msg += " in <select>[name='" + aElement.name + "']";
+			else if (aElement.className) msg += " in <select>[class='" + aElement.className + "']";
+			else msg += " in <select>";
+			msg += ", because it is not multiple.";
+			throw new Error(msg);
+		}
+
+		for (i = 0; i < values.length; i++) {
+			values[i] = (values[i] === null || values[i] === undefined) ? '' : ''+values[i];
+		}
+
+		selected = [].concat(values);
+
+		for (i = 0; i < aElement.options.length; i++) {
+			matched = false;
+
+			for (j = 0; j < values.length; j++) {
+				if (aElement.options[i].value === values[j]) {
+					matched = true;
+					selected.splice(j, 1);
+					break;
+				}
+			}
+
+			if (matched) {
+				aElement.options[i].selected = true;
+				if (!multiple) break;
+			}
+			else {
+				aElement.options[i].selected = false;
+			}
+		}
+
+		if (selected.length > 0) {
+			msg = "Could not select <option>[value='" + selected[0] + "'] by value";
+			if (aElement.id) msg += " in <select>[id='" + aElement.id + "']";
+			else if (aElement.name) msg += " in <select>[name='" + aElement.name + "']";
+			else if (aElement.className) msg += " in <select>[class='" + aElement.className + "']";
+			else msg += " in <select>";
+			msg += ".";
+
+			throw new Error(msg);
+		}
+	};
+
 	if (_createGlobals) {
 		ObjectUtils.defineNamespace('Solarfield.Ok');
 		Solarfield.Ok.DomUtils = DomUtils;
