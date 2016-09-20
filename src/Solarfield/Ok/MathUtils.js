89e552e291b8e3d118b6907da0506d9d8cb06425
mike <mikecaines@gmail.com>
2016-09-19 21:01:30 -0300

    MathUtils::rectsIntersect(), ::rectsNearestSide

--- src/Solarfield/Ok/MathUtils.js
+++ src/Solarfield/Ok/MathUtils.js
@@ -45,6 +45,32 @@
 		return Math.round(aFloat * offset) / offset;
 	};
 
+	/**
+	 * Whether or not two rectangles intersect.
+	 * @param r1 Rectangle1 as [x1, y1, x2, y2]
+	 * @param r2 Rectangle2
+	 * @returns {boolean}
+	 */
+	MathUtils.rectsIntersect = function (r1, r2) {
+		return r1[0]<r2[2] && r1[2]>r2[0] && r1[1]<r2[3] && r1[3]>r2[1];
+	};
+
+	/**
+	 * The absolute minimum distance between two rectangles.
+	 * Sizes which overlap, or are contained within the other rectangle, are considered zero.
+	 * @param r1 Rectangle1 as [x1, y1, x2, y2]
+	 * @param r2 Rectangle2
+	 * @returns {number}
+	 */
+	MathUtils.rectsNearestSide = function(r1, r2) {
+		return Math.max(
+			Math.max(0, r1[0]-r2[2]),
+			Math.max(0, r2[0]-r1[2]),
+			Math.max(0, r1[1]-r2[3]),
+			Math.max(0, r2[1]-r1[3])
+		);
+	};
+
 	if (_createGlobals) {
 		ObjectUtils.defineNamespace('Solarfield.Ok');
 		Solarfield.Ok.MathUtils = MathUtils;
