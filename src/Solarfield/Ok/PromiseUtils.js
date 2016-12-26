613b094d388c1c38da68a18eced43cd415dbf42c
mike <mikecaines@gmail.com>
2016-12-26 15:13:56 -0400

    make PromiseUtils.allSettled() more useful
    Now always resolves, with an object containing a resolved list, and a
     rejected list. Breaking change.

--- src/Solarfield/Ok/PromiseUtils.js
+++ src/Solarfield/Ok/PromiseUtils.js
@@ -29,26 +29,28 @@
 	};
 
 	/**
-	 * Similar to Promise.all(), but settles once all passed promises are settled (i.e. no fail-fast behaviour).
-	 * Rejects with an array of rejected values from aArray.
-	 * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Promise.all_fail-fast_behaviour
+	 * Creates a promise that resolves once all specified promises have settled.
+	 * The promise will not reject.
 	 * @param {Array} aArray Array of Promise's.
-	 * @returns {Promise}
+	 * @returns {Promise.<{resolved:Array, rejected:Array}>}
 	 */
 	PromiseUtils.allSettled = function (aArray) {
-		var errors = [];
-		var values = [];
+		var rejected = [];
+		var resolved = [];
 
 		return Promise.all(aArray.map(function (promise) {
-			return promise.then(function (v) {
-				values.push(v);
+			return Promise.resolve(promise).then(function (v) {
+				resolved.push(v);
 			})
 			.catch(function (e) {
-				errors.push(e);
+				rejected.push(e);
 			});
 		}))
 		.then(function () {
-			return errors.length > 0 ? Promise.reject(errors) : Promise.resolve(values);
+			return {
+				resolved: resolved,
+				rejected: rejected,
+			};
 		});
 	};
 
