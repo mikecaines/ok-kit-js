9ed44027b6f07a800d5d1a075c34375f8e8adb41
mike <mikecaines@gmail.com>
2017-02-03 20:12:35 -0400

    var's to let's

--- src/Solarfield/Ok/StructUtils.js
+++ src/Solarfield/Ok/StructUtils.js
@@ -25,21 +25,20 @@
 	"use strict";
 
 	/**
-	 * @class Solarfield.Ok.StructUtils
+	 * @class StructUtils
 	 */
-	var StructUtils = function () {
+	const StructUtils = function () {
 		throw new Error("Class is abstract.");
 	};
 
 	StructUtils.scout = function (aObject, aPath, aSeparator) {
-		var separator = aSeparator || '.';
-		var steps = (aPath+'').split(separator);
-		var node, i, result;
+		const separator = aSeparator || '.';
+		const steps = (aPath+'').split(separator);
 
-		result = [false, null];
+		let result = [false, null];
 
-		node = aObject;
-		for (i = 0; i < steps.length; i++) {
+		let node = aObject;
+		for (let i = 0; i < steps.length; i++) {
 			if (node != null && typeof node == 'object' && steps[i] in node) {
 				if (i == steps.length - 1) {
 					result = [true, node[steps[i]]];
@@ -68,15 +67,15 @@
 	};
 
 	StructUtils.set = function (aObject, aPath, aValue, aSeparator) {
-		var separator = aSeparator || '.';
-		var steps = (aPath+'').split(separator);
-		var node, i;
+		const separator = aSeparator || '.';
+		const steps = (aPath+'').split(separator);
 
 		if (!(aObject != null && typeof aObject == 'object')) {
 			throw "aObject must be an Object.";
 		}
 
-		node = aObject;
+		let node = aObject;
+		let i;
 		for (i = 0; i < steps.length - 1; i++) {
 			if (!(node[steps[i]] != null && typeof node[steps[i]] == 'object' && steps[i] in node)) {
 				node[steps[i]] = {};
@@ -89,15 +88,15 @@
 	};
 
 	StructUtils.pushSet = function (aObject, aPath, aValue, aSeparator) {
-		var separator = aSeparator || '.';
-		var steps = (aPath+'').split(separator);
-		var node, i;
+		const separator = aSeparator || '.';
+		const steps = (aPath+'').split(separator);
 
 		if (!(aObject != null && typeof aObject == 'object')) {
 			throw "aObject must be an Object.";
 		}
 
-		node = aObject;
+		let node = aObject;
+		let i;
 		for (i = 0; i < steps.length - 1; i++) {
 			if (!(node[steps[i]] != null && typeof node[steps[i]] == 'object' && steps[i] in node)) {
 				node[steps[i]] = {};
@@ -114,23 +113,21 @@
 	};
 
 	StructUtils.merge = function (aObject1, aObject2) {
-		var v1, v2, merged, k, arr;
-
-		v1 = StructUtils.isVector(aObject1);
-		v2 = StructUtils.isVector(aObject2);
+		const v1 = StructUtils.isVector(aObject1);
+		const v2 = StructUtils.isVector(aObject2);
 
 		if ((v1 && !v2) || (!v1 && v2)) {
 			throw "Cannot merge vector and non-vector.";
 		}
 
-		merged = v1 ? [] : {};
+		const merged = v1 ? [] : {};
 
-		arr = v2 ? aObject2 : aObject1;
-		for (k in arr) {
+		const arr = v2 ? aObject2 : aObject1;
+		for (let k in arr) {
 			merged[k] = arr[k];
 		}
 
-		for (k in aObject2) {
+		for (let k in aObject2) {
 			if (
 				(k in merged)
 				&& merged[k] != null
@@ -150,10 +147,8 @@
 	};
 
 	StructUtils.mergeInto = function (aObject1, aObject2) {
-		var v1, v2, k;
-
-		v1 = StructUtils.isVector(aObject1);
-		v2 = StructUtils.isVector(aObject2);
+		const v1 = StructUtils.isVector(aObject1);
+		const v2 = StructUtils.isVector(aObject2);
 
 		if ((v1 && !v2) || (!v1 && v2)) {
 			throw "Cannot merge vector and non-vector.";
@@ -162,12 +157,12 @@
 		//if aObject2 is a vector, replace contents of aObject1 with contents of aObject2
 		if (v2) {
 			aObject1.splice(0);
-			for (k in aObject2) {
+			for (let k in aObject2) {
 				aObject1[k] = aObject2[k];
 			}
 		}
 
-		for (k in aObject2) {
+		for (let k in aObject2) {
 			if (
 				(k in aObject1)
 				&& aObject1[k] != null
@@ -189,23 +184,20 @@
 	 * @type {Function}
 	 */
 	StructUtils.assign = Object.assign || function (aObject1, aObjectN) {
-			var i, k;
-
-			for (i = 1; i < arguments.length; i++) {
-				for (k in arguments[i]) {
-					aObject1[k] = arguments[i][k];
-				}
+		for (let i = 1; i < arguments.length; i++) {
+			for (let k in arguments[i]) {
+				aObject1[k] = arguments[i][k];
 			}
+		}
 
-			return aObject1;
-		};
+		return aObject1;
+	};
 
 	StructUtils.search = function (aObject, aPath, aValue, aStrict) {
-		var strict = aStrict != null ? aStrict : false;
-		var k, value;
+		const strict = aStrict != null ? aStrict : false;
 
-		for (k in aObject) {
-			value = StructUtils.get(aObject[k], aPath);
+		for (let k in aObject) {
+			const value = StructUtils.get(aObject[k], aPath);
 
 			if ((strict == false && value == aValue) || (strict == true && value === aValue)) {
 				return k;
@@ -216,15 +208,13 @@
 	};
 
 	StructUtils.find = function (aObject, aPath, aValue, aStrict) {
-		var k = StructUtils.search(aObject, aPath, aValue, aStrict);
+		const k = StructUtils.search(aObject, aPath, aValue, aStrict);
 		return (k !== null) ? aObject[k] : null;
 	};
 
 	StructUtils.isVector = function (aObj) {
-		var i, k;
-
-		i = 0;
-		for (k in aObj) {
+		let i = 0;
+		for (let k in aObj) {
 			if (k != i) return false;
 			i++;
 		}
